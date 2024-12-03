7d795675363318900c487439f8394da9f5bad268
khushi <bajpaikv@rknec.edu>
2024-12-04 01:14:01 +0530

    Adding coding Question of DSA solved in Java.

--- src/OOPS/Encapsulation.java
+++ src/OOPS/Encapsulation.java
@@ -1,5 +1,5 @@
 package OOPS;        // Acess modifiers-->public->koi bhi iss info ko access kr sakta h,
-// private-> cannot be accessed outside the class or by the other classes , these can vbe accesed using getters and setters
+// private-> cannot be accessed outside the class or by the other classes , these can be accesed using getters and setters
 // protected->usse apne package me saari cheeze access kr sakti h agar dusre package me krna ho toh sirf sub classes access kr sakti h ,
 // default-> already exist hota h kuch nhi lagaya toh default hota hai it can be accessible by anyone within the package
 
